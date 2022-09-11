216f1c2142518cdf566d0f04940369144b0edc79
Naamail <naamail@post.bgu.ac.il>
2022-09-11 10:05:24 +0300

    organized clientSumUp in folder

---
+++ Day 4/NavBarExample_ClientSumUP/ActiveNav.js
@@ -0,0 +1,11 @@
+const ActivePage = window.location.pathname;
+console.log(ActivePage);
+
+const activeNav = document.querySelectorAll('nav a').forEach(
+    MyLinks => {
+        if (MyLinks.href.includes(`${ActivePage}`)) {
+            MyLinks.classList.add('Active');
+        }
+
+    }
+)
