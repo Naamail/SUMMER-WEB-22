aa7ea3809ae20991a2c0b2b340618006cbd16902
Naamail <naamail@post.bgu.ac.il>
2022-09-23 01:03:07 +0300

    added pug folder

--- Day_6/PUG/server.js
+++ Day_6/PUG/server.js
@@ -1,26 +1,24 @@
 const { application } = require('express');
 const express = require('express');
 const path = require('path');
-
 const port = 8080;
-
 const app = express();
-
+//set up pubkic directory for static files
 app.use(express.static(path.join(__dirname, "public")));
-
+// set up views directory for html/pug files
 app.set('views',path.join(__dirname,'views'));
+// set up view engine as pug
 app.set('view engine', 'pug');
 
- app.get('/', (req,res)=>{
-    res.render('index', {
-        var1: "Hi all",
-        var2: "This is a pug demo"
-    });
-    //res.send('hello PUG');
- });
+app.get('/', (req,res)=>{
+   res.render('index', {
+      var1: "Hi all",
+      var2: "This is a pug demo"
+   });
+   //res.send('hello PUG');
+});
 
-
- app.get('/page2', (req,res)=>{
+app.get('/page2', (req,res)=>{
    res.render('page2', {
        var1: "page 2",
        var2: "page 2 page 2 page 2"
