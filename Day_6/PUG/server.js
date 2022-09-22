f1063a2b4ade49ffee6a33207476f0b9a3f5b678
Naamail <naamail@post.bgu.ac.il>
2022-09-22 23:13:01 +0300

    added pug folder

--- Day_6/PUG/server.js
+++ Day_6/PUG/server.js
@@ -6,6 +6,8 @@ const port = 8080;
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, "public")));
+
 app.set('views',path.join(__dirname,'views'));
 app.set('view engine', 'pug');
 
@@ -16,6 +18,22 @@ app.set('view engine', 'pug');
     });
     //res.send('hello PUG');
  });
+
+
+ app.get('/page2', (req,res)=>{
+   res.render('page2', {
+       var1: "page 2",
+       var2: "page 2 page 2 page 2"
+   });
+});
+
+app.get('/page3', (req,res)=>{
+   res.render('page3', {
+       var1: "page 3",
+       var2: "page 3 page 3 page 3"
+   });
+});
+
  app.listen(port, ()=>{
     console.log("server is running on port " + port);
  }
